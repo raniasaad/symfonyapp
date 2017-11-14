3ec8a06236a42379fc94a5af12db64c2339c2930
ihebsaad <ihebsaad@gmail.com>
2017-11-14 12:34:57 +0100

    autoload

--- app/autoload.php
+++ app/autoload.php
@@ -6,8 +6,7 @@ use Composer\Autoload\ClassLoader;
 /**
  * @var ClassLoader $loader
  */
-//$loader = require __DIR__.'/../vendor/autoload.php';
-$loader = require '/var/www/vhosts/localhost.localdomain/httpdocs/Apps/symfonyapp/vendor/autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
