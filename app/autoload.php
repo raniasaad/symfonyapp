40d53e243ab68d02079f9c4974ee8a68e7011a0b
ihebsaad <ihebsaad@gmail.com>
2017-11-14 12:33:03 +0100

    clear security

--- app/autoload.php
+++ app/autoload.php
@@ -6,7 +6,8 @@ use Composer\Autoload\ClassLoader;
 /**
  * @var ClassLoader $loader
  */
-$loader = require __DIR__.'/../vendor/autoload.php';
+//$loader = require __DIR__.'/../vendor/autoload.php';
+$loader = require '/var/www/vhosts/localhost.localdomain/httpdocs/Apps/symfonyapp/vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
